0fabbe8af61cb591ccb17da96bdc07f9d0041ed1
huy.luong <luonganhhuy5@gmail.com>
2024-09-11 14:40:57 +0700

    update Format

--- src/test/java/Training/Topic_00_Format.java
+++ src/test/java/Training/Topic_00_Format.java
@@ -1,10 +1,8 @@
 package Training;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
