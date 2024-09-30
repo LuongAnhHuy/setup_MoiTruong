b60c525ab6670b6f5c7e63f3e8bb0a099d54a364
huy.luong <luonganhhuy5@gmail.com>
2024-09-30 17:20:56 +0700

    update code

--- src/test/java/Training/Demo_Java.java
+++ src/test/java/Training/Demo_Java.java
@@ -1,7 +1,33 @@
 package Training;
 
-public class Demo_Java {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Demo_Java {
+    static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        System.out.println("Nhap so nguyen n: ");
+        int n = scanner.nextInt();
+
+//        if (n % 2 != 0) {
+//            n++;
+//        }
+
+//        for (int i = n; i <= 100; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println("So chia het cho 2 la: " + i);
+//            }
+//        }
+
+        int i = n;
+        while (i < 100) {
+            if (i % 2 == 0) {
+                System.out.println("So chia het cho 2 la: " + i);
+            }
+            i++;
+//            i += 2;
+//            System.out.println("So chia het cho 2 la: " + i);
+        }
     }
 }
